a45db0c0851ed677a95634e3920008f6d3d2eee9
zhangdavids <1269969739@qq.com>
2016-04-11 17:25:57 +0800

    1c

--- code.py
+++ code.py
@@ -1,14 +1,27 @@
-
-
 import web
+
+render=web.template.render('templates/')
 
 urls=(
 	'/','index'
 )
 
+movies=[
+    {
+        'title':'Forrest Gump',
+        'year':'1994',
+    },
+    {
+        'title':'Titanic',
+        'year':'1997',
+    },
+]
+
 class index:
-	def GET(self):
-		return "Hello,tony!"
+    def GET(self):
+        
+        return render.index(movies)
+
 
 
 if __name__ == '__main__':
